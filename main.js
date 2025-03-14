c60d56802afa48e7bffe216fbae863b9238dc04d
GMesmo <guimachado94@gmail.com>
2025-03-14 15:37:13 -0300

    Implementação de JS das aulas

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ function Carro(modelo, fabricante, anoModelo, anoFabricacao) {
 
 const carroDoJoao = new Carro("Fiesta", "Ford", 2020, 2019);
 const carroDaMaria = new Carro("Ka", "Ford", 2021, 2020);
+
+const nome = "Guilherme";
+const idade = 30;
+const ehMaiorDeIdade = true;
+const conhecimentos = ["HTML", "CSS", "JavaScript"];
+
+const pessoa = {
+  nome: nome,
+  idade: idade,
+  ehMaiorDeIdade: ehMaiorDeIdade,
+  conhecimentos: conhecimentos,
+};
+
+console.log(pessoa.nome);
+console.log(pessoa["nome"]);
