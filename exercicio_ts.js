9f1147af6730b317b259674956daa39d7f1ce64e
GMesmo <guimachado94@gmail.com>
2025-06-09 17:51:03 -0300

    Implementação do exercício

---
+++ exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(num1, num2) {
+    return num1 * num2;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplica(5, 5));
+console.log(saudacao("Guilherme"));
---
+++ exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplica(num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+console.log(multiplica(5, 5));
+console.log(saudacao("Guilherme"));
